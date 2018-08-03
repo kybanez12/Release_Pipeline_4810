84b6698bb6eba558025ace051672fe7c279e8f5e
kybanez12 <kybanez12@gmail.com>
2018-08-03 13:36:09 +1000

    new api project

---
+++ ReleasePipeDemoApp/ReleasePipeDemoApplication/Areas/HelpPage/SampleGeneration/SampleDirection.cs
@@ -0,0 +1,11 @@
+namespace ReleasePipeDemoApplication.Areas.HelpPage
+{
+    /// <summary>
+    /// Indicates whether the sample is used for request or response
+    /// </summary>
+    public enum SampleDirection
+    {
+        Request = 0,
+        Response
+    }
+}
